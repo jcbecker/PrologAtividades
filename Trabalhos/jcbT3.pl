faa734530d78f341c756cf64e3a8d43a055e14fb
jcbecker <joaocarlosbecker666@gmail.com>
2017-05-09 15:43:42 -0300

    maior parte feita

--- Trabalhos/jcbT3.pl
+++ Trabalhos/jcbT3.pl
@@ -8,7 +8,7 @@ Faça predicados para responder a perguntas sobre este banco de dados:
 * Quais disciplinas um professor/aluno leciona/assiste
 * Quais são os horários livres de um professor/aluno
 * Quais são os horários comuns entre um professor e um aluno
-* Quais são os horários comuns entre um professor e um aluno
+* Quais são os horários livres comuns entre um professor e um aluno
 * Quantas desciplinas são ministradas na parte da manhã
 * Invente 3 outras perguntas interessantes
 
@@ -71,7 +71,7 @@ disciplina('IA', 2, [1411100040, 1411100022, 1411100001, 1411100015],[1, 8]).
 disciplina('OptII', 4, [1, 2, 3, 1411100001],[2, 3]).
 disciplina('OptI', 5, [3, 1411100001, 1411100040, 1411100022],[4, 9]).
 disciplina('TCC', 3, [1, 1411100001, 1411100040, 1411100022, 666],[10, 11]).
-disciplina('OptIII', 2, [1, 5, 6, 1411100001, 1411100040, 1411100022],[22, 23]).
+disciplina('OptIII', 2, [1, 5, 6, 1411100040, 1411100022],[22, 23]).
 
 %Fim do banco de dados----------------------------------------------------------
 
@@ -79,7 +79,10 @@ disciplina('OptIII', 2, [1, 5, 6, 1411100001, 1411100040, 1411100022],[22, 23]).
 testall:-write('Imprimindo todos alunos e suas respectivas disciplinas:'), nl, printallalunos,nl, nl,
     write('Imprimindo todos professores e suas respectivas disciplinas:'), nl, printprofessores, nl, nl,
     write('Os horários disponíveis de cada professor são: '),nl,  printProfessoresLivres, nl, nl,
-    write('Os alunos com seus horários livres: '), nl, printAlunosLivres, nl, nl.
+    write('Os alunos com seus horários livres: '), nl, printAlunosLivres, nl, nl,
+    write('Horarios em comun com o aluno Marco '), nl, horasemcomun, nl, nl,
+    write('Horarios livres em comun com o aluno Vladimir '), nl, horaslivrescm, nl, nl,
+    write('Quantidade de disciplinas no matutino '), hmmatutino(N), write(N), nl, nl.
 
 
 
@@ -92,6 +95,9 @@ printProfessoresLivres:- forall(professor(P, Nome), (write('Professor '),write(N
 
 printAlunosLivres:- forall(aluno(A, Nome), (write('Aluno '),write(Nome), write(' Tem livre os horários :'),freeha(A, L), write(L), nl)).
 
+horasemcomun:- forall(professor(A, Nome), (write('Professor '), write(Nome), write(' Tem os seguintes horarios em comun com Marco '), hcpa(A, 1411100022, L), write(L), nl)).
+
+horaslivrescm:- forall(professor(A, Nome), (write('Professor '), write(Nome), write(' Tem os seguintes horarios livres em comun com Vladimir '), hlcpa(A, 14111000001, L), write(L), nl)).
 %Quais disciplinas um professor/aluno leciona/assiste
 alunoassiste(X, Dlist):- aluno(X, _), findall(Disc, (disciplina(Disc, _, As, _), member(X, As)), Dlist).
 
@@ -108,3 +114,16 @@ freehorarios(Lin, Lout):-findall(A , hr(A), L), subtract(L, Lin, Lout).
 %Horarios livres para cada aluno A, freeha(A, FreeList).
 freeha(A, O):-hrdoaluno(A, L), freehorarios(L, O).
 hrdoaluno(X, L):- findall(Hr, (disciplina(_, _, Alist, Hr), member(X, Alist)), Ca), flatten(Ca, L).
+
+%Horarios em comum entre um professor P e um aluno A, hcpa(P, A, ResulList).
+hcpa(P, A, L):-hrdoprofessor(P, Plist), hrdoaluno(A, Alist), intersection(Alist, Plist, L).
+
+%Horarios livres em comum entre um professor P e um aluno A, hlcpa(P, A, ResulList).
+hlcpa(P, A, L):-freehp(P, Plist), freeha(A, Alist), intersection(Alist, Plist, L).
+
+%Quantas desciplinas são ministradas na parte da manhã
+hmmatutino(N):-findall(Disc, (disciplina(Disc, _, _, Hr),horarioTeste(Hr)), L),length(L, N). 
+horarioTeste([X|_]):-X<10, !.
+horarioTeste([_|T]):-horarioTeste(T).
+
+
