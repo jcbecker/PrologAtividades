3cb82bd7ffa16a8008c8ef404a356f03e22ef5ac
jcbecker <joaocarlosbecker666@gmail.com>
2017-06-06 14:40:41 -0300

    comentar predicados e remover warnings

--- Trabalhos/t5/t5.pl
+++ Trabalhos/t5/t5.pl
@@ -12,7 +12,8 @@ Usando tabuleiro como uma lista de 9 posições representado por
 	  start.
 
 
-start:-%loadDB,
+start:-
+	loadDB,
 	printOptions,
 	repeat,
 		play(player,1,[v,v,v,v,v,v,v,v,v], [player]),
@@ -23,7 +24,7 @@ start:-%loadDB,
 
 
 %loadDB Predicado que carrega a base de dados
-loadDB:-open('animalBase.txt', read, S),repeat, mReadLine(S), close(S), !.%DEBUG:trocar nome do arquivo
+loadDB:-open('playedDB.txt', read, S),repeat, mReadLine(S), close(S), !.
 mReadLine(S):-read(S, In),(In=end_of_file | assert(In), fail).
 
 printOptions:-write('Bem vindo ao jogo da velha!\nPara selecionar uma célula de jogada, pressione o número da célula\n'),
@@ -31,16 +32,17 @@ printOptions:-write('Bem vindo ao jogo da velha!\nPara selecionar uma célula de
 	write('(1) | (2) | (3)\n(4) | (5) | (6)\n(7) | (8) | (9)'),nl, nl.
 
 
-gameover:-%DEBUG: aqui preciso salvar no banco de dados o resultado
+gameover:-
 	write('Deseja continuar jogando? Pressione \'s\' para sim, outra letra caso contrário: '), get_single_char(C), put(C), nl,
 	char_code(D, C),
     ((D == 's') -> fail; true).
 
-mWriteDB:-write('Escrevendo jogadas na base de dados'), nl.
+mWriteDB:-write('Escrevendo jogadas na base de dados'), nl,
+	open('playedDB.txt', write, S), forall(played(X, Y), (writeq(S,played(X, Y)), write(S, '.'), nl(S))), close(S).
 
 %H: uma lista com o histórico das jogadas, na partida atual
 play(J1, N, T, H) :-
-	boardPrint(T),%DEBUG: passa para o makeMove do player
+	%boardPrint(T),%DEBUG: passa para o makeMove do player, OBS: feito
 	makeMove(N,J1,T,P1, H),
 	append(H, [P1], H1),
 	executa(J1,P1,T,T1),
@@ -48,31 +50,32 @@ play(J1, N, T, H) :-
 	 play(J2,N1,T1, H1)), !.
 
 makeMove(N, player, T, P, _):-
+	boardPrint(T),
 	repeat, mWriteList(['Jogada ', N, ' - vez do player: ']),
 	mReadPosition(P), isFree(1,P,T,v), !.
 
 makeMove(N, computer, T, P, Ahist):-
-	repeat, mWriteList(['Jogada ', N, ' - vez do computador: ']), nl,
+	repeat, mWriteList(['Jogada ', N, ' - vez do computador: ']),
 	fitnessList(T, L, Ahist),
 	length(L, U),
 	random(0, U, Rnd),
 	nth0(Rnd,L,[F, P]),
-	
+	write(P), nl,
 	isFree(1,P,T,v), 
-	nl, !.
+	!.
 	%write(T), !.
 
 %Ahist: histórico de jogadas na partida atual
 fitnessList(T, L, Ahist):-fitnessList(T, [], 1, L, Ahist).
-fitnessList([], A, _, P, _):- mCutList(A, P) 
-	,write('Sem Corte: '), write(A), nl, write('Com corte: '), write(P), nl
+fitnessList([], A, _, P, _):- mCutList(A, P) %descomentar a linha de baixo caso queira ver a lista de fitness
+	%, nl, write('Sem Corte: '), write(A), nl, write('Com corte: '), write(P), nl
 	.
 fitnessList([v|R], A, N, L, Ahist):-
 	append(Ahist, [N], AhistN),
 	fitnessCalc(AhistN, Fit),
 	append(A, [[Fit, N]], A1),
 	N1 is N+1,
-	fitnessList(R, A1, N1, L, Ahist).%DEBUG: aqui preciso calcular fitness
+	fitnessList(R, A1, N1, L, Ahist).
 fitnessList([_|R], A, N, L, Ahist):-
 	append(A, [[-1, N]], A1),
 	N1 is N+1,
@@ -80,14 +83,18 @@ fitnessList([_|R], A, N, L, Ahist):-
 
 
 fitnessCalc(L, Fit):-
-	write('Calculando Fit, Hist: '), write(L),nl,
-	findall(played(X, Y) , (played(X, Y), prefix(L, X)), OutList),
-	write('Prefixos: '), write(OutList),
-	length(OutList, N),
-	((N == 0) -> Fit is 0.5; Fit is 2),
-	nl.
+	%write('Calculando Fit, Hist: '), write(L),nl,
+	findall(Y , (played(X, Y), prefix(L, X)), OutList),
+	%write('Prefixos: '), write(OutList),
+	length(OutList, N), %write(' De comprimento N: '), write(N),
+	((N == 0) -> Fit is 0.5; fitness2Calc(OutList, N, Fit)).
+	
+fitness2Calc(OutList, N, Fit):- mNumCalc(OutList, 0, Num), Fit is Num/N.
 
-
+mNumCalc([], Na, Na).
+mNumCalc([player|R], Na, Num):- mNumCalc(R, Na, Num).%soma 0
+mNumCalc([computer|R], Na, Num):- Na1 is Na+2,  mNumCalc(R, Na1, Num).%soma 2
+mNumCalc([tie|R], Na, Num):- Na1 is Na+1, mNumCalc(R, Na1, Num).%soma 1
 
 mCutList(L, P):-mMaxList(L, M),
 	%mWriteList(['Na lista ', L, ' A maior fitness é: ', M]),
@@ -149,4 +156,3 @@ victoryTest(J,[_,J,_,_,J,_,_,J,_]).
 victoryTest(J,[_,_,J,_,_,J,_,_,J]).
 victoryTest(J,[J,_,_,_,J,_,_,_,J]).
 victoryTest(J,[_,_,J,_,J,_,J,_,_]).
-
